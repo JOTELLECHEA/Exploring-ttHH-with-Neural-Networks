932811ddbd94e29f5a0df6139fcb6ee4cb44be2c
JOTELLECHEA <jtellece@ucsc.edu>
2021-05-10 12:07:50 -0700

    makes plots

--- dataExport.py
+++ dataExport.py
@@ -1,6 +1,9 @@
 import uproot
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 tree = "OutputTree" # The name of the tree object in ROOT.
 
@@ -23,16 +26,16 @@ HighLevel = [
 ]
 
 ### Low Level variables START -
-type = ["flav", "pT", "eta", "phi", "b", "c"]
+types = ["flav", "pT", "eta", "phi", "btag"]
 LeptonVAR = []
 JetVAR = []
 jetcount = 10 # This number can be changed. The range is 0-21. Currnetly only focusing on 10 first 10 jets. 
-for i in range(4):
+for i in range(3):
     for j in range(3):
-        LeptonVAR.append("lepton" + str(j + 1) + type[i])
-for i in range(1, 6):
+        LeptonVAR.append("lepton" + str(j + 1) + types[i])
+for i in range(1, 5):
     for j in range(jetcount):
-        JetVAR.append("jet" + str(j + 1) + type[i])
+        JetVAR.append("jet" + str(j + 1) + types[i])
 ###                                               -END
 
 phase = 3 # Keep at 3 to include all data, modify to limit the data by feature type.
@@ -54,26 +57,54 @@ numBranches = len(branches) - 2
 signal = uproot.open("/home/jtellece/neural_networks/data/new_TTHH.root")[tree] 
 
 # The [:5] is reading in the first 5 events. 
-df_signal = signal.arrays(branches, library="pd")[:5] # Using Uproot4 which is installed in slugpu 
-# df_signal = signal.pandas.df(branches)[:5] # Using Uproot3
+# df_signal = signal.arrays(branches, library="pd")[:5] # Using Uproot4 which is installed in slugpu 
+df_signal = signal.pandas.df(branches)[:5] # Using Uproot3
 # df_signal.head() # This will print out the first few events in table form. 
 
 # This saves the pandas dataframe above into a csv file. 
 # This is optional you can just df_signal as a n-d numpy array. 
-df_signal.to_csv('TTHH.csv') 
+# df_signal.to_csv('TTHH.csv') 
 
 bkgTTBB = uproot.open("/home/jtellece/neural_networks/data/new_TTBB.root")[tree]
 df_bkgTTBB = bkgTTBB.pandas.df(branches)[:5]
-df_bkgTTBB.to_csv('TTBB.csv')
+# df_bkgTTBB.to_csv('TTBB.csv')
 
 
 bkgTTH = uproot.open("/home/jtellece/neural_networks/data/new_TTH.root")[tree]
 df_bkgTTH = bkgTTH.pandas.df(branches)[:5]
-df_bkgTTH.to_csv('TTH.csv')
+# df_bkgTTH.to_csv('TTH.csv')
 
 
 bkgTTZ = uproot.open("/home/jtellece/neural_networks/data/new_TTZ.root")[tree]
 df_bkgTTZ = bkgTTZ.pandas.df(branches)[:5]
-df_bkgTTZ.to_csv('TTZ.csv')
+# df_bkgTTZ.to_csv('TTZ.csv')
 
-print('DONE')
+xx =[]
+yy=[]
+zz=[]
+for i in range(5):
+    xx.append(df_bkgTTZ['jet1eta'].values[i])
+    yy.append(df_bkgTTZ['jet1phi'].values[i])
+    zz.append(df_bkgTTZ['jet1pT'].values[i])
+
+x = np.linspace(-3,3,100)
+y = np.linspace(-3,3,100)
+nums = []
+
+for i in range(100):
+
+    nums.append([])
+
+    for j in range(100):
+
+        nums[i].append(0)
+
+nums[0][2] = zz[0]
+
+#setup the 2D grid with Numpy
+X, Y = np.meshgrid(x, y)
+
+#now just plug the data into pcolormesh, it's that easy!
+plt.pcolormesh(X, Y, nums,shading='auto')
+plt.colorbar() #need a colorbar to show the intensity scale
+plt.show() #boom
