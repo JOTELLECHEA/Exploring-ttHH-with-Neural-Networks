6096ee551a4184c0d2fc4a7102e3f7cd16ed7336
JOTELLECHEA <jtellece@ucsc.edu>
2021-08-12 15:51:23 -0700

    removed unsude import

--- slug.py
+++ slug.py
@@ -10,7 +10,6 @@ import math
 import matplotlib
 import numpy as np
 import pandas as pd
-import seaborn as sn
 #matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
